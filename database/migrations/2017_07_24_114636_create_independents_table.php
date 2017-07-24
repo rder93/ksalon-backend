4612d899ce6bf9329b7598fc35c50dd719ed6bad
Jesús Torres <jesustc17@gmail.com>
2017-07-24 07:24:52 -0430

    Se agrego el modelo, migracion y contralador 'Indepent' que es con quien se manejaran los profesionales independientes, se elimno el atributo user_id de la migracion professional

--- database/migrations/2017_07_24_114636_create_independents_table.php
+++ database/migrations/2017_07_24_114636_create_independents_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfessionalsTable extends Migration
+class CreateIndependentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateProfessionalsTable extends Migration
      */
     public function up()
     {
-        Schema::create('professionals', function (Blueprint $table) {
+        Schema::create('independents', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre');
-            $table->string('foto');
-            $table->string('direccion');
-            $table->integer('lounge_id');
+            $table->integer('user_id');
+            $table->integer('cedula');
+            $table->integer('foto');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateProfessionalsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('professionals');
+        Schema::dropIfExists('independents');
     }
 }
