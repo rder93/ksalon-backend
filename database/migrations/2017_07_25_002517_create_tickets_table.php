1fc0e855edf4c839c9535a8b53464eeee191eaa7
Jonathan Cuotto <jonathancuotto@gmail.com>
2017-07-25 01:07:47 -0400

    agregada migracion de tickets table

---
+++ database/migrations/2017_07_25_002517_create_tickets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('owner_id');
+            $table->integer('ticket_id');
+            $table->string('content');
+            $table->string('subject');
+            $table->integer('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
